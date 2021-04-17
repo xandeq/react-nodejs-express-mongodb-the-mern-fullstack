81fb1945aec8274fca3dc99bb887625edc740d6b
xandeq <xandeq@gmail.com>
2021-04-17 10:47:30 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+import ListaObjetivos from "./components/ListaObjetivos/ListaObjetivos";
+import NovoObjetivo from "./components/NovoObjetivo/NovoObjetivo";
 
 function App() {
+  const [listaObjetivos, setListaObjetivos] = useState([
+    {
+      id: "cg1",
+      texto: "Finalizar o curso",
+    },
+    {
+      id: "cg2",
+      texto: "Aprender tudo sobre o tema principal",
+    },
+    {
+      id: "cg3",
+      texto: "Ajudar os outros estudantes do curso",
+    },
+  ]);
+
+  function adicionarNovoObjetivoHandler(novoObjetivo) {
+    setListaObjetivos(listaObjetivos.concat(novoObjetivo));
+    listaObjetivos.push(novoObjetivo);
+    console.log(listaObjetivos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="objetivos-do-curso">
+      <h2>Course Goals</h2>
+      <NovoObjetivo adicionarObjetivo={adicionarNovoObjetivoHandler} />
+      <ListaObjetivos objetivos={listaObjetivos} />
     </div>
   );
 }
